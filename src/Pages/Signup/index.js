d3c50a2d760cbd9cc5007ee7d83fbd0219c355b3
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-09-19 23:57:30 -0400

    got unform working with javascript

--- src/Pages/Signup/index.js
+++ src/Pages/Signup/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { KeyboardAvoidingView, View, Image, Platform, ScrollView } from 'react-native'
+import React, { useRef } from 'react'
+import {
+    KeyboardAvoidingView,
+    View,
+    Image,
+    Platform,
+    ScrollView,
+} from 'react-native'
+import { Form } from '@unform/mobile';
 import { Container, Title } from './styles'
-import LogoImage from '../../assets/logo.png';
+import LogoImage from '../../assets/logo.png'
+import Input from '../../components/Input/input'
 
-
-export default function Signup() {
+const Signup = () => {
+    const formRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
     return (
         <>
             <KeyboardAvoidingView
@@ -14,15 +24,23 @@ export default function Signup() {
             >
                 <ScrollView
                     keyboardShouldPersistTaps="handled"
-                    contentContainerStyle={{ flex: 1 }}>
+                    contentContainerStyle={{ flex: 1 }}
+                >
                     <Container>
                         <Image source={LogoImage} />
                         <View>
                             <Title>Create Account</Title>
                         </View>
+                        <Form ref={formRef} onSubmit={() => { console.log('submitting') }}>
+                            <Input name="name" />
+                            <Input name="email" ref={emailRef} />
+                            <Input name="password" ref={passwordRef} />
+                        </Form>
                     </Container>
                 </ScrollView>
             </KeyboardAvoidingView>
         </>
     )
 }
+
+export default Signup
